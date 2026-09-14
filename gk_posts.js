8e32445400575ce5b4811a47225dd138484758a4
sgsinstaboost <vipanimationyt@gmail.com>
2026-09-14 12:17:02 +0530

    Create gk_posts.js

---
+++ gk_posts.js
@@ -0,0 +1,27 @@
+// GENERAL KNOWLEDGE (GK) POSTS
+const gkData = {
+  id: "gk",
+  title: "सामान्य ज्ञान (Static GK)",
+  icon: "🌍",
+  chapters: [
+    {
+      id: "history",
+      name: "भारतीय इतिहास (Indian History)",
+      tests: [
+        {
+          id: "gk_hist_1",
+          name: "History - Test 1",
+          timeMinutes: 5,
+          questions: [
+            {
+              q: "पानीपत का प्रथम युद्ध किस वर्ष हुआ था?",
+              options: ["1526", "1556", "1761", "1530"],
+              ans: 0,
+              exp: "पानीपत की पहली लड़ाई 21 अप्रैल 1526 को बाबर और इब्राहिम लोदी के बीच लड़ी गई थी।"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
